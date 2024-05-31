367c690eed78a6a70a7efb9e16038de92add8feb
chloelkl <99401053+chloelkl@users.noreply.github.com>
2024-05-31 17:04:16 +0800

    updated theme, inserted our theme colours and typography deafult font to poppins

--- client/src/themes/MyTheme.js
+++ client/src/themes/MyTheme.js
@@ -2,14 +2,23 @@
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
-    palette: {
-        primary: {
-            main: '#388e3c',
-        },
-        secondary: {
-            main: '#f4511e',
-        }
+  palette: {
+    primary: {
+      light: '#94C4BB',
+      main: '#87AEA6',
+    },
+    secondary: {
+      light: '#FFFFFF',
+      main: '#F1FFE8',
+      dark: '#000000'
     }
+  },
+  typography: {
+    fontFamily: [
+      'Poppins',
+      'Arial'
+    ].join(','),
+  }
 });
 
 export default theme;
