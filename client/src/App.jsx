e8cf6b52913791c7e412c41022db7fa726c39339
chloelkl <99401053+chloelkl@users.noreply.github.com>
2024-05-31 17:06:43 +0800

    updated main app file, removed pratical things

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,27 @@
 import './App.css';
-import { Container, AppBar, Toolbar, Typography } from '@mui/material';
+import { Container, AppBar, Toolbar, Typography, IconButton } from '@mui/material';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import MyTheme from './themes/MyTheme';
-import Tutorials from './pages/Tutorials';
-import AddTutorial from './pages/AddTutorial';
-import EditTutorial from './pages/EditTutorial';
-import MyForm from './pages/MyForm';
+import './fonts/Poppins.css';
+
+// import pages here
+import Homepage from './pages/Homepage';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
       <ThemeProvider theme={MyTheme}>
-        <AppBar position="static" className="AppBar">
-          <Container>
-            <Toolbar disableGutters={true}>
-              <Link to="/">
-                <Typography variant="h6" component="div">
-                  Learning
-                </Typography>
-              </Link>
-              <Link to="/tutorials" ><Typography>Tutorials</Typography></Link>
-            </Toolbar>
-          </Container>
-        </AppBar>
+        {/* navigation bar */}
+        <Navbar />
 
         <Container>
           <Routes>
-            <Route path={"/"} element={<Tutorials />} />
-            <Route path={"/tutorials"} element={<Tutorials />} />
-            <Route path={"/addtutorial"} element={<AddTutorial />} />
-            <Route path={"/edittutorial/:id"} element={<EditTutorial />} />
-            <Route path={"/form"} element={<MyForm />} />
+            {/* add paths for each page */}
+            <Route path={"/"} element={<Homepage />} />
+            <Route path={"/homepage"} element={<Homepage />} />
+
           </Routes>
         </Container>
       </ThemeProvider>
