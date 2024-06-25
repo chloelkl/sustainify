ab242ab0bbe6658560c0f7a3a43506c7aea55ed7
Jayyy419 <Jayyy419@hotmail.com>
2024-06-25 14:10:58 +0800

    Revert "After merging index.js"
    This reverts commit a1cf0253370569bb365f3c2c0e760e2502fcf529.
    
    Revert commit a1cf0253370569bb365f3c2c0e760e2502fcf529

--- server/index.js
+++ server/index.js
@@ -33,17 +33,11 @@ app.use("/event", eventRoute);
 const testRoute = require('./routes/test');
 app.use("/test", testRoute);
 
-const userRoute = require('./routes/user');
-app.use("/user", userRoute);
-
-const adminRoute = require('./routes/admin');
-app.use("/admin", adminRoute);
-
 // Start server after synchronising the DB files under models folder
 const db = require('./models');
 db.sequelize.sync({ alter: false })
     .then(() => {
-        let port = process.env.APP_PORT;
+        let port = process.env.APP_PORT || 5000;
         app.listen(port, () => {
             console.log(`⚡ Server running on http://localhost:${port}`);
         });
