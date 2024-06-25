8772418599cfcfda9ff640195b602358ab11f0a6
chloelkl <99401053+chloelkl@users.noreply.github.com>
2024-06-25 14:31:39 +0800

    fixed navbar conflict bug

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,26 +4,12 @@ import { Link } from 'react-router-dom';
 import { styled } from '@mui/system';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
-// using mui to style navbar
->>>>>>> parent of 5c1f6df (Completed admin main)
-const StyledToolbar = styled(Toolbar)({
-  display: 'flex',
-  justifyContent: 'space-around',
-<<<<<<< HEAD
-  alignItems: 'center',
-=======
 // using mui to style navbar
 const StyledToolbar = styled(Toolbar)({
   display: 'flex',
   justifyContent: 'space-around',
   height: '100px'
->>>>>>> f08394f678773cfa212aed1ab5587775fee344c6
-=======
->>>>>>> parent of ffc2fde (Improved system overview)
 });
 
 const NavLinks = styled('div')({
@@ -33,19 +19,8 @@ const NavLinks = styled('div')({
   justifyContent: 'space-around'
 });
 
-<<<<<<< HEAD
+
 const Logo = styled('img')({
-<<<<<<< HEAD
-  height: '40px',
-=======
->>>>>>> f08394f678773cfa212aed1ab5587775fee344c6
-=======
-const Logo = styled(Typography)({
-<<<<<<< HEAD
->>>>>>> parent of ffc2fde (Improved system overview)
-=======
-  // flexGrow: 1,
->>>>>>> parent of 5c1f6df (Completed admin main)
   textAlign: 'center',
   height: '100px',
   '&:hover': {
@@ -77,15 +52,7 @@ function Navbar() {
             <Typography>EVENTS</Typography>
           </StyledLink>
           <StyledLink to="/">
-<<<<<<< HEAD
-<<<<<<< HEAD
-            <Logo src="/src/assets/Logo_XBG.png" alt="LOGO" /> {/* Use your logo image path */}
-=======
-            <Logo src="../../public/Logo.png" alt="sustainify" />
->>>>>>> f08394f678773cfa212aed1ab5587775fee344c6
-=======
-            <Logo>LOGO</Logo>
->>>>>>> parent of ffc2fde (Improved system overview)
+            <Logo src="Logo.png" alt="sustainify" />
           </StyledLink>
           
 
