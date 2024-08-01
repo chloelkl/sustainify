7042e1fa0e91d31d56568f0e8e0527eb5313f72a
chloelkl <99401053+chloelkl@users.noreply.github.com>
2024-08-01 14:58:52 +0800

    added check completion logic to past challenges

--- client/src/pages/challenges/user/PastChallenges.jsx
+++ client/src/pages/challenges/user/PastChallenges.jsx
@@ -3,6 +3,7 @@ import theme from '../../../themes/MyTheme.js'
 import { styled } from '@mui/system';
 import { Typography } from "@mui/material";
 import dayjs from 'dayjs';
+import { useAuth } from '../../../context/AuthContext.jsx';
 
 import AssignmentOutlinedIcon from '@mui/icons-material/AssignmentOutlined';
 import AssessmentOutlinedIcon from '@mui/icons-material/AssessmentOutlined';
@@ -62,6 +63,7 @@ const Cont = styled('div')({
   });
 
 function PastChallenges() {
+  const { user } = useAuth();
   const [challenges, setChallenges] = useState([]);
 
   useEffect(() => {
@@ -83,6 +85,16 @@ function PastChallenges() {
     const yesterday = dayjs().subtract(1, 'day').startOf('day');
     return challengeDate.isBefore(yesterday) || challengeDate.isSame(yesterday, 'day');
   }).sort((a, b) => dayjs(b.date).diff(dayjs(a.date)));;
+
+  const checkCompletion = async (challengeId, userId) => {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/challenge/checkCompletion?userId=${userId}&challengeId=${challengeId}`);
+      const result = await response.json();
+      return result
+    } catch (error) {
+      console.error('Error checking completion of challenge ' + challengeId + ':' + error);
+    }
+  }
 
   
 
@@ -114,7 +126,7 @@ function PastChallenges() {
                   <Typography width="15%">{dayjs(chal.date).format('DD/MM/YYYY')}</Typography>
                   <Typography width="60%">{chal.challenge}</Typography>
                   <div style={{ 'width': '10%', display: 'flex', justifyContent: 'center' }}>
-                    
+                    { checkCompletion(chal.id, user.userID).completed ? "yes" : "no" }
                   </div>
                 </Cont>
                 <hr style={{ width: '95%', margin: '0 auto' }} />
