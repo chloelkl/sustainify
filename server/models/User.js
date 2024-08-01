ffce94c48e01a0325e9d212345a0d06cc9933932
chloelkl <99401053+chloelkl@users.noreply.github.com>
2024-08-01 10:55:46 +0800

    added users tablename, created association with challenge

--- server/models/User.js
+++ server/models/User.js
@@ -93,6 +93,13 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.TEXT,
             allowNull: true
         }
+    }, {
+      tableName: 'users'
     });
+
+    User.associate = (models) => {
+      User.belongsToMany(models.Challenge, { through: "UserChallenges", foreignKey: 'user' });
+    };
+  
     return User;
 };
