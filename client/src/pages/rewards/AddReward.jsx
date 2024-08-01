66eda9318ce0a26c71d9bc2fdd03b3cc3eef24fa
Sardonic <foojiwei1052@gmail.com>
2024-08-01 22:50:10 +0800

    Ensure the font-end file upload works
    Signed-off-by: Sardonic <foojiwei1052@gmail.com>

--- client/src/pages/rewards/AddReward.jsx
+++ client/src/pages/rewards/AddReward.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Typography, TextField, Button, Grid } from '@mui/material';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
@@ -10,6 +10,7 @@ const allowedPoints = [100, 300, 400, 500, 600]
 
 function AddReward() {
   const navigate = useNavigate();
+  const [rewardImage, setImageFile] = useState(null);
 
   const formik = useFormik({
     initialValues: {
@@ -29,6 +30,9 @@ function AddReward() {
         .integer('Points must be an integer'),
     }),
     onSubmit: (values) => {
+      if (rewardImage) {
+        values.rewardImage = rewardImage;
+      }
       http.post('/reward', values)
         .then((res) => {
           console.log(res.data);
@@ -41,12 +45,32 @@ function AddReward() {
     },
   });
 
+  const onFileChange = (e) => {
+    let file = e.target.files[0];
+    if (file) {
+
+      let formData = new FormData();
+      formData.append('file', file);
+      http.post('/file/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      })
+        .then((res) => {
+          setImageFile(res.data.filename);
+        })
+        .catch(function (error) {
+          console.log(error.response);
+        });
+    }
+  };
+
   return (
-    <Box sx={{ bgcolor: 'white', p: 2, borderRadius: 8, marginTop: '100px', paddingLeft: '120px', paddingRight: '120px', paddingTop: '50px', paddingBottom: '50px'}}>
+    <Box sx={{ bgcolor: 'white', p: 2, borderRadius: 8, marginTop: '100px', paddingLeft: '120px', paddingRight: '120px', paddingTop: '50px', paddingBottom: '50px' }}>
       <Typography variant="h5" sx={{ mb: 2, fontSize: '40px', fontWeight: 'bold', textAlign: 'center' }}>
         Add Reward
       </Typography>
-      <form onSubmit={formik.handleSubmit}>
+      <Box component="form" onSubmit={formik.handleSubmit}>
         <Grid container spacing={6} alignItems="center">
           <Grid item xs={8}>
             <TextField
@@ -62,7 +86,7 @@ function AddReward() {
               helperText={formik.touched.rewardname && formik.errors.rewardname}
             />
           </Grid>
-          <Grid item  xs={4}>
+          <Grid item xs={4}>
             <TextField
               fullWidth
               margin="normal"
@@ -77,15 +101,39 @@ function AddReward() {
               helperText={formik.touched.points && formik.errors.points}
             />
           </Grid>
-        </Grid>
+          </Grid>
+            {
+                rewardImage && (
+                  <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center',
+                    height: '200px', marginBottom: '40px', marginTop: '20px', 
+                    paddingTop: '25px', paddingBottom: '25px', border: '2px solid lightgrey', 
+                    borderRadius: '8px', overflow: 'hidden'}}>
+                    <img
+                      alt="reward"
+                      src={`${import.meta.env.VITE_FILE_BASE_URL}${rewardImage}`}
+                      style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'contain' }}
+                    />
+                  </Box>
+                )
+              }
+              <Box sx={{ mt: 2, fontWeight: 'bold', textAlign: 'center' }}>
+              <Button class="button-upload" variant="contained" component="label">
+                Upload Image
+                <input hidden accept="image/*" multiple type="file"
+                  onChange={onFileChange} />
+              </Button>
+            </Box> 
+              
+
         <Box sx={{ mt: 2, fontWeight: 'bold', textAlign: 'center' }}>
           <Button variant="contained" color="secondary" type="submit">
             Add
           </Button>
         </Box>
-      </form>
+      </Box>
     </Box>
   );
 }
 
 export default AddReward;
+   
