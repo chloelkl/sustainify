272c202d62a3711018b18c780432bd802cc28001
AndrewAce <andrewkangmingyi@gmail.com>
2024-08-12 00:03:37 +0800

    Improved forum admin page with forum rendering and viewing

--- client/src/pages/forum/ForumAdmin.jsx
+++ client/src/pages/forum/ForumAdmin.jsx
@@ -8,11 +8,15 @@ import dayjs from "dayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { Button, Box, TextField, Typography } from "@mui/material";
+import { Button, Box, TextField, Typography, Modal, CardMedia, IconButton } from "@mui/material";
 import http from "../../http";
 import { useAuth } from "../../context/AuthContext";
+import { Clear } from "@mui/icons-material";
 import ForumCard from "./ForumCard";
+import { Link } from "react-router-dom";
 import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
+
+const dateFormat = "D MMM YYYY";
 
 const SelectDate = styled(DatePicker)({
   width: "30%",
@@ -43,12 +47,14 @@ const ForumAdmin = () => {
   const { authToken } = useAuth();
   const [forums, setForums] = useState([]);
   const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedItem, setSelectedItem] = useState(null);
   const [filteredForums, setFilteredForums] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
   const [startDate, setStartDate] = useState(dayjs().subtract(7, "day"));
   const [endDate, setEndDate] = useState(dayjs());
   const [responseMessage, setResponseMessage] = useState("");
   const [responseType, setResponseType] = useState("");
+  const [taint, setTaint] = useState(false);
 
   useEffect(() => {
     getForums();
@@ -153,6 +159,14 @@ const ForumAdmin = () => {
     setSelectedItem(item);
   };
 
+  const handleClose = () => {
+    setSelectedItem(null);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedItem(null);
+  };
+
   return (
     <>
       <Box sx={{ marginTop: "3rem" }}>
@@ -195,25 +209,144 @@ const ForumAdmin = () => {
             onChange={(newValue) => {
               setSelectedDate(newValue);
               filterForumsByDate(newValue);
+              setTaint(true);
             }}
             renderInput={(params) => <TextField {...params} />}
             maxDate={dayjs()}
           />
         </LocalizationProvider>
       </Box>
-      {filteredForums.length > 0 && (
+      {filteredForums.length > 0 && taint && (
       <Box sx={{ marginBottom: "3rem" }}>
         <Masonry columnsCount={4} gutter="10px">
           {filteredForums.map((item) => (
-            <ForumCard key={item.id} item={item} />
+            <ForumCard 
+            key={item.id} 
+            item={item} 
+            onCardClick={() => handleCardClick(item)}/>
           ))}
         </Masonry>
       </Box>
     )}
-    <Box sx={{ marginBottom: "3rem" }}>
+    {selectedItem && (
+        <Modal
+          open={Boolean(selectedItem)}
+          onClose={handleClose}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Box
+            sx={{
+              width: "80%",
+              maxWidth: 900,
+              height: "auto",
+              minHeight: "300px",
+              backgroundColor: "white",
+              borderRadius: 2,
+              boxShadow: 24,
+              display: "flex",
+              overflow: "hidden",
+              position: "relative",
+            }}
+          >
+            <Box sx={{ width: "40%", height: "auto" }}>
+              <CardMedia
+                component="img"
+                image={
+                  selectedItem.image
+                    ? `${import.meta.env.VITE_API_URL}/${selectedItem.image}`
+                    : "https://images.pexels.com/photos/355508/pexels-photo-355508.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                }
+                alt={selectedItem.title}
+                sx={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                }}
+              />
+            </Box>
+            <Box sx={{ width: "60%", padding: 3, position: "relative" }}>
+              <IconButton
+                aria-label="save"
+                sx={{
+                  position: "absolute",
+                  top: 16,
+                  right: 16,
+                }}
+                onClick={handleCloseModal}
+              >
+                <Clear />
+              </IconButton>
+              <Typography variant="h4" sx={{ mb: 2, fontWeight: "bold" }}>
+                {selectedItem.title}
+              </Typography>
+              <Box
+                sx={{
+                  flex: 1,
+                  mb: 2,
+                }}
+              >
+                <Typography
+                  variant="body1"
+                  sx={{
+                    whiteSpace: "pre-line",
+                    wordBreak: "break-word", // Ensures long words break and wrap
+                  }}
+                >
+                  {selectedItem.description}
+                </Typography>
+              </Box>
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                sx={{ position: "absolute", bottom: 16, left: 16, right: 16 }}
+              >
+                <Link
+                  to={`/forum/by/${selectedItem.userId}`}
+                  style={{
+                    textDecoration: "none", // Remove default underline
+                    color: "inherit", // Inherit color from parent
+                    display: "inline-block", // Make the link inline-block for better styling
+                    position: "relative", // Needed for the pseudo-element
+                    paddingBottom: "2px", // To adjust space for underline effect
+                  }}
+                >
+                  <Typography
+                    variant="body2"
+                    sx={{
+                      color: "textSecondary",
+                      transition: "color 0.3s, textDecoration 0.3s", // Smooth transition
+                      "&:hover": {
+                        color: "primary.main", // Change text color on hover
+                      },
+                    }}
+                  >
+                    {selectedItem.User?.username}
+                  </Typography>
+                </Link>
+                <Typography variant="body2" color="textSecondary">
+                  {dayjs(selectedItem.createdAt).format(dateFormat)}
+                </Typography>
+              </Box>
+            </Box>
+          </Box>
+        </Modal>
+      )}
+    {!filteredForums.length && taint && (
+      <Box sx={{ marginBottom: "3rem" }}>
       <Box><SentimentVeryDissatisfiedIcon/></Box>
         <Typography>No Forums Created.</Typography>
       </Box>
+    )}
+    {!taint && (
+      <Box sx={{ marginBottom: "3rem" }}>
+      <Box><SentimentVeryDissatisfiedIcon/></Box>
+        <Typography>Generate the Forums</Typography>
+      </Box>
+    )}
     </>
   );
 };
